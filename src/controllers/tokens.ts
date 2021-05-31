44e66c4d9cc4e9ed8d4617fc740ba37ead5a8bd8
Oleg <79220300111@yandex.ru>
2021-05-31 09:01:40 +0500

    validation by arguments

--- src/controllers/tokens.ts
+++ src/controllers/tokens.ts
@@ -8,7 +8,6 @@ import {
 } from 'class-validator'
 import { ERC20ContractData } from '../interfaces/erc20ContractData'
 
-// Validator class
 class ValidTokenERC20 implements ERC20ContractData {
   @IsString()
   name: string
@@ -39,24 +38,7 @@ class ValidTokenERC20 implements ERC20ContractData {
 export default class TokenController {
   // Validation for add token ERC20 request
   @Post('/addTokenERC20')
-  async validAddTokenERC20(@Body() body: any) {
-    const newTokenERC20 = new ValidTokenERC20()
-
-    newTokenERC20.name = body.name
-    newTokenERC20.symbol = body.symbol
-    newTokenERC20.rate = Number.parseInt(body.rate)
-    newTokenERC20.receiver = body.receiver
-    newTokenERC20.decimals = Number.parseInt(body.decimals)
-    newTokenERC20.timed = body.timed
-    newTokenERC20.whitelist = body.whitelist
-    newTokenERC20.maxTokens = Number.parseInt(body.maxTokens)
-
-    // resValidate is an array of validation errors
-    const resValidate = await validate(newTokenERC20)
-    if (resValidate.length > 0) {
-      return resValidate
-    } else {
-      return body
-    }
+  async validAddTokenERC20(@Body() body: ValidTokenERC20) {
+    return body
   }
 }
