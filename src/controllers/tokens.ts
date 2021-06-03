b37992f39e43cd756c128075c2f25248448abbfa
Oleg <79220300111@yandex.ru>
2021-06-03 10:23:59 +0500

    Create an endpoint with validation object for builder ERC721

--- src/controllers/tokens.ts
+++ src/controllers/tokens.ts
@@ -7,6 +7,7 @@ import {
   IsEthereumAddress,
 } from 'class-validator'
 import { ERC20ContractData } from '@/interfaces/erc20ContractData'
+import { ERC721ContractData } from '@/interfaces/erc721ContractData'
 
 class ValidTokenERC20 implements ERC20ContractData {
   @IsString()
@@ -34,10 +35,32 @@ class ValidTokenERC20 implements ERC20ContractData {
   maxTokens: number
 }
 
+class ValidTokenERC721 implements ERC721ContractData {
+  @IsString()
+  name: string
+
+  @IsString()
+  symbol: string
+
+  @IsNumber()
+  rate: number
+
+  @IsString()
+  tokenURI: string
+
+  @IsString()
+  receiver: string
+}
+
 @Controller('/tokens')
 export default class TokenController {
   @Post('/addTokenERC20')
   async validAddTokenERC20(@Body() body: ValidTokenERC20) {
+    return body
+  }
+
+  @Post('/addTokenERC721/:id')
+  async validAddTokenERC721(@Body() body: ValidTokenERC721) {
     return body
   }
 }
---
+++ src/interfaces/erc721ContractData.ts
@@ -0,0 +1,7 @@
+export interface ERC721ContractData {
+  name: String
+  symbol: String
+  rate: Number
+  tokenURI: String
+  receiver: String
+}
